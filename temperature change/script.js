912f3ec29ec374bfd8f51308eddea1210337b503
THESOURCEC0D3 <jonathantamunoiduari@gmail.com>
2025-05-29 15:03:11 +0100

    made a simple temperature converter

---
+++ temperature change/script.js
@@ -0,0 +1,48 @@
+function convert(){
+let inputField = document.querySelector('input')
+let inputvalue = document.querySelector('input').value;
+let convertion = document.querySelector('select').value;
+let resultField = document.querySelector('#convertionResult')
+let result;
+
+resultField.value = "";
+
+if (inputvalue === "" || isNaN(inputvalue)) {
+  alert("please enter a valid temperature");
+  inputField.focus();
+  return;
+}
+
+
+
+if(convertion === ""){
+    alert('Please select a convertion type')
+    return;
+}
+
+let input = Number(inputvalue);
+
+if(isNaN(input)){
+    alert('Please enter a valid number')
+    return;
+}
+if(convertion === "F to C"){
+ result = (input-32) * 5/9; 
+ resultField.value = `${result.toFixed(3)} °C`;
+} else {
+ result = input * 9/5 + 32;
+ resultField.value = `${result.toFixed(3)} °F`;
+}
+
+// document.querySelector('#convertionResult').value = result.toFixed(3)
+}
+function resetform(){
+    //Clear input field
+    document.querySelector('input').value = "";
+
+    //Resest select to default option
+    document.querySelector('select').selectedIndex = 0;
+
+    //Clear result field
+    document.querySelector('#convertionResult').value = "";
+}
